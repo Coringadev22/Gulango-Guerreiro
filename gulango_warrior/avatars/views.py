e18197e8bb678157ec1cd439355fc53a283259bc
Coringadev22 <lukaslopes.coelho@icloud.com>
2025-06-12 22:00:16 +0100

    Add perfil_avatar view and template

--- gulango_warrior/avatars/views.py
+++ gulango_warrior/avatars/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+from .models import Avatar
+
+
+@login_required
+def perfil_avatar(request):
+    """Exibe os dados do avatar do usuário logado."""
+    avatar = Avatar.objects.get(user=request.user)
+    context = {
+        "avatar": avatar,
+    }
+    return render(request, "avatars/perfil.html", context)
+
