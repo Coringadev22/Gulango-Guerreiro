92db15019ab8f419c55b4bef49e8f382d927c13c
Coringadev22 <lukaslopes.coelho@icloud.com>
2025-06-13 16:04:12 +0100

    Add unit tests for remaining apps

--- gulango_warrior/accounts/tests.py
+++ gulango_warrior/accounts/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import CustomUser
+
+
+class CustomUserModelTests(TestCase):
+    """Tests for the :class:`CustomUser` model."""
+
+    def test_default_not_instructor(self):
+        user = CustomUser.objects.create_user(username="hero", password="123")
+        self.assertFalse(user.is_instructor)
+
+    def test_create_instructor(self):
+        user = CustomUser.objects.create_user(
+            username="mentor", password="123", is_instructor=True
+        )
+        self.assertTrue(user.is_instructor)
--- gulango_warrior/avatars/tests.py
+++ gulango_warrior/avatars/tests.py
@@ -1,3 +1,37 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+from accounts.models import CustomUser
+from .models import Avatar
+
+
+class AvatarModelTests(TestCase):
+    def test_ganhar_xp_sobe_nivel(self):
+        user = CustomUser.objects.create_user(username="player", password="123")
+        avatar = Avatar.objects.create(user=user)
+
+        avatar.ganhar_xp(50)
+        self.assertEqual(avatar.xp_total, 50)
+        self.assertEqual(avatar.nivel, 1)
+
+        avatar.ganhar_xp(60)
+        avatar.refresh_from_db()
+        self.assertEqual(avatar.nivel, 2)
+
+    def test_str(self):
+        user = CustomUser.objects.create_user(username="knight", password="123")
+        avatar = Avatar.objects.create(user=user, nivel=3)
+        self.assertEqual(str(avatar), "knight - Nível 3")
+
+
+class AvatarViewSecurityTests(TestCase):
+    def setUp(self):
+        self.user = CustomUser.objects.create_user(username="a", password="b")
+        Avatar.objects.create(user=self.user)
+
+    def test_views_require_login(self):
+        for name in ["perfil_avatar", "ranking_geral", "destaque_conquistas"]:
+            url = reverse(name)
+            response = self.client.get(url)
+            self.assertEqual(response.status_code, 302)
+            self.assertIn("/accounts/login/", response["Location"])
