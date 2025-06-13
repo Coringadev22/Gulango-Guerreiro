0ed835517aaa2a30b15b1379dce681da82f4868a
Coringadev22 <lukaslopes.coelho@icloud.com>
2025-06-13 01:07:33 +0100

    Add regiao field to Course model

--- gulango_warrior/courses/models.py
+++ gulango_warrior/courses/models.py
@@ -2,10 +2,26 @@ from django.db import models
 
 # Create your models here.
 
-class Course(models.Model): 
+class Course(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
     instructor = models.ForeignKey('accounts.CustomUser', on_delete=models.CASCADE)
+
+    REGIAO_FLORESTA_GO = "floresta_de_go"
+    REGIAO_FORTALEZA_RUSTICA = "fortaleza_rustica"
+    REGIAO_PLANICIES_JULIANAS = "planicies_julianas"
+
+    REGIAO_CHOICES = [
+        (REGIAO_FLORESTA_GO, "floresta_de_go"),
+        (REGIAO_FORTALEZA_RUSTICA, "fortaleza_rustica"),
+        (REGIAO_PLANICIES_JULIANAS, "planicies_julianas"),
+    ]
+
+    regiao = models.CharField(
+        max_length=20,
+        choices=REGIAO_CHOICES,
+        default=REGIAO_FLORESTA_GO,
+    )
     created_at = models.DateTimeField(auto_now_add=True)
 
 class Lesson(models.Model):
