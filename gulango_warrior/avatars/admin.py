20e8f8de65df24e9d128b6744dccf6c6b738a5d7
Coringadev22 <lukaslopes.coelho@icloud.com>
2025-06-13 17:21:06 +0100

    Customiza admin de Avatar

--- gulango_warrior/avatars/admin.py
+++ gulango_warrior/avatars/admin.py
@@ -5,4 +5,12 @@ from .models import Avatar
 
 @admin.register(Avatar)
 class AvatarAdmin(admin.ModelAdmin):
-    list_display = ("user", "classe", "nivel", "xp_total", "moedas")
+    list_display = ("get_user_name", "classe", "nivel", "xp_total", "moedas")
+    list_filter = ("classe",)
+    ordering = ("-xp_total",)
+
+    def get_user_name(self, obj):
+        """Return the avatar owner's full name or username."""
+        return obj.user.get_full_name() or obj.user.username
+
+    get_user_name.short_description = "Nome do usuário"
