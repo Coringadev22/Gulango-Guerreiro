c2f7a5968497a32eb71b5cd0204814d73cb93154
Coringadev22 <lukaslopes.coelho@icloud.com>
2025-06-13 17:32:43 +0100

    Configure admin for HistoricoDialogo

--- gulango_warrior/courses/admin.py
+++ gulango_warrior/courses/admin.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
 from .models import Course, Lesson, NPC, HistoricoDialogo
 
+
+class HistoricoDialogoAdmin(admin.ModelAdmin):
+    list_display = ("npc", "usuario", "pergunta", "resposta", "data")
+    search_fields = ("usuario__username", "npc__nome")
+    ordering = ["-data"]
+
 admin.site.register(Course)
 admin.site.register(Lesson)
 admin.site.register(NPC)
-admin.site.register(HistoricoDialogo)
+admin.site.register(HistoricoDialogo, HistoricoDialogoAdmin)
