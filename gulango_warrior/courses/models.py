4ae3e31ea0714ae8a5554117c36b5f2f45c5f1de
Coringadev22 <lukaslopes.coelho@icloud.com>
2025-06-13 18:20:34 +0100

    Add linguagem field to Course

--- gulango_warrior/courses/models.py
+++ gulango_warrior/courses/models.py
@@ -7,6 +7,22 @@ class Course(models.Model):
     description = models.TextField()
     instructor = models.ForeignKey('accounts.CustomUser', on_delete=models.CASCADE)
 
+    LING_GOLANG = "golang"
+    LING_RUST = "rust"
+    LING_JULIA = "julia"
+
+    LINGUAGEM_CHOICES = [
+        (LING_GOLANG, "golang"),
+        (LING_RUST, "rust"),
+        (LING_JULIA, "julia"),
+    ]
+
+    linguagem = models.CharField(
+        max_length=20,
+        choices=LINGUAGEM_CHOICES,
+        default=LING_GOLANG,
+    )
+
     REGIAO_FLORESTA_GO = "floresta_de_go"
     REGIAO_FORTALEZA_RUSTICA = "fortaleza_rustica"
     REGIAO_PLANICIES_JULIANAS = "planicies_julianas"
