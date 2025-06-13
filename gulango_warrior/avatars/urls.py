ff7bbe5cd0d27bb8b3efa4ad63e050356ac3e5ee
Coringadev22 <lukaslopes.coelho@icloud.com>
2025-06-13 12:29:13 +0100

    Add destaque_conquistas view and template

--- gulango_warrior/avatars/urls.py
+++ gulango_warrior/avatars/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import perfil_avatar, ranking_geral
+from .views import perfil_avatar, ranking_geral, destaque_conquistas
 
 urlpatterns = [
     path('perfil/', perfil_avatar, name='perfil_avatar'),
     path('ranking/', ranking_geral, name='ranking_geral'),
+    path('conquistas/', destaque_conquistas, name='destaque_conquistas'),
 ]
